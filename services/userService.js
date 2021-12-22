105fa8c1595191420394f2d7f7b81bf8a08dddfa
Vovi4 <vovi4.vladzyo@gmail.com>
2021-12-22 15:53:20 +0200

    Implement methods to work with user

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,46 @@ class UserService {
 
     // TODO: Implement methods to work with user
 
+    getAllUsers() {
+        const users = UserRepository.getAll();
+        if(!users) {
+            throw new Error("Users not found!");
+        }
+        return users;
+    }
+    
+    getUser(search) {
+        const user = UserRepository.getOne(search);
+        if(!user) {
+            throw new Error("User not found!");
+        }
+        return user;
+    }
+    
+    create(data) {
+        const user = UserRepository.create(data);
+        if(!user) {
+            throw new Error("User not created!");
+        }
+        return user;
+    }
+    
+    update(id, dataToUpdate) {
+        const user = UserRepository.update(id, dataToUpdate);
+        if(!user) {
+            throw new Error("User not updated!");
+        }
+        return user;
+    }
+    
+    delete(id) {
+        const user = UserRepository.delete(id);
+        if(!user) {
+            throw new Error("User not deleted!");
+        }
+        return user;
+    }
+
     search(search) {
         const item = UserRepository.getOne(search);
         if(!item) {
